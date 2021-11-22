c6571ed8c1aa2f702247055a2062bb905e69adbd
katiacreator <kcygibson@gmail.com>
2021-11-22 17:29:35 -0500

    added span to score and high score divs on index.html
    added submit button event listener, compare guess function, and displaySCore functionality

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 'use strict'
-
-//* Constants */
+/* Constants */
 const secret = Math.trunc(Math.random() * 100) + 1
-// console.log("🚀 ~ file: app.js ~ line 4 ~ secret", secret)
-const guessDiv = document.querySelector('.guess')//grab .value to get number-will be a string so have to convert
-// console.log("🚀 ~ file: app.js ~ line 7 ~ guessDiv:", guessDiv)
+console.log(secret)
 const secretDiv = document.querySelector('#secret')
-// console.log("🚀 ~ file: app.js ~ line 9 ~ secretDiv:", secretDiv)
+// console.log("🚀 ~ file: app.js ~ line 7 ~ secretDiv", secretDiv)
 const displayDiv = document.querySelector('#display')
-// console.log("🚀 ~ file: app.js ~ line 11 ~ displayDiv", displayDiv)
 const scoreDiv = document.querySelector('.score')
-const highscoreDiv = document.querySelector('.high-score')
-
-//* Local Variables that will change throughout game */
-let displayText = displayDiv.textContent
-// console.log("🚀 ~ file: app.js ~ line 20 ~ displayText", displayText)
-let secretDivText = secretDiv.textContent
-// console.log("🚀 ~ file: app.js ~ line 22 ~ secretDivText", secretDivText)
-let score = Number(scoreDiv.textContent)
-// console.log("🚀 ~ file: app.js ~ line 17 ~ score", score)
-let highScore = Number(highscoreDiv.textContent)
-// console.log("🚀 ~ file: app.js ~ line 19 ~ highScore", highScore)
+const scoreSpan = document.querySelector('#score')
+// console.log("🚀 ~ file: app.js ~ line 9 ~ scoreSpan", scoreSpan)
 
 
-//* Cached Element References */
-const btns = document.querySelectorAll('.btns')//Nodelist reference for game reset
+/* Global Variables */
+let score = 0;
+// console.log("🚀 ~ file: app.js ~ line 8 ~ score", score)
+let highScore;
+let guess;
+
+
+/* Cached Element References */
 const submit = document.querySelector('#submit')
-const start = document.querySelector('#start')//hide and/or disable once clicked, show at beginning of the game
-const reset = document.querySelector('#reset')//show at end of game, disable and hide once clicked
+// console.log("🚀 ~ file: app.js ~ line 13 ~ submit", submit)
+const start = document.querySelector('#start')
+const reset = document.querySelector('#reset')
+const btns = document.querySelectorAll('.btns')
 
-//* Functions */
+
+/* Functions */
+submit.addEventListener('click', function(){
+  guess = Number(document.querySelector('.guess').value)
+  console.log("🚀 ~ file: app.js ~ line 21 ~ submit.addEventListener ~ guess", guess)
+  compareGuess()
+})
+
+const compareGuess = function(){
+  if (guess < secret) {
+    displayDiv.textContent = `${guess} is too low`
+  } else if(guess > secret) {
+    displayDiv.textContent = `${guess} is too high`
+  } else {
+    displayScore()
+  }
+}
+const displayScore = function (){
+  displayDiv.textContent = `${guess} is the secret number!`
+  secretDiv.textContent = `${secret}`
+  score++
+  console.log(score)
+  scoreSpan.textContent = score
+}
